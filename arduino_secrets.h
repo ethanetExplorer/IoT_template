73231ee55ca1343e79a84fdd8f87723abf526d66
Ethan Lim <107737645+ethanetExplorer@users.noreply.github.com>
2025-09-14 10:57:25 +0800

    first commit

---
+++ arduino_secrets.h
@@ -0,0 +1,18 @@
+
+/* WiFi credentials. Set password to "" for open networks. */
+#define WIFI_SSID "Ultra Fast Wi-Fi Pro Max"  // Replace with the name of your hotspot
+#define WIFI_PASS "p@ss23rd_h3r3!"            // Replace with your password
+
+/* Fill in information from Blynk Device Info here.
+   Read More: https://bit.ly/BlynkSimpleAuth
+*/
+#define BLYNK_TEMPLATE_ID "TMPxxxxxx"
+#define BLYNK_TEMPLATE_NAME "MQTT Demo"
+#define BLYNK_AUTH_TOKEN "cuuVI8vxXev2tWvcoYfQmb7XqCDZnWye"
+
+/* Change the default Blynk server. Applicable for users with a white label plan. */
+//#define BLYNK_MQTT_BROKER       "blynk.cloud"
+//#define BLYNK_MQTT_PORT         8883
+
+/* Disable Secure TLS connection. Only use it for testing purposes. */
+//#define BLYNK_MQTT_UNSECURE
